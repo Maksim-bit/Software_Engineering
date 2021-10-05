034357b56e4a10985d720ac7f6b2095ad1e573d9
Maksim-bit <maks24071998@mail.ru>
2021-10-05 15:43:24 +0300

    End

--- src/Algorithm1.java
+++ src/Algorithm1.java
@@ -3,14 +3,12 @@ import java.time.Instant;
 import java.time.LocalTime;
 
 public class Algorithm1 {
-    public static double expt(double b, int n) throws InterruptedException {
-        Thread.sleep(1);
+    public static double expt(double b, int n) {
         if (n == 0) return 1;
         return b * expt(b, n - 1);
     }
 
-    public static double exptCycle(double b, int n) throws InterruptedException {
-        Thread.sleep(1);
+    public static double exptCycle(double b, int n) {
         if (n == 0) return 1;
         double result = 1;
         for (; n > 0; n--) {
@@ -20,35 +18,23 @@ public class Algorithm1 {
     }
 
     public static void test1() throws InterruptedException {
-        Clock clock = Clock.systemDefaultZone();
-        Instant instant = clock.instant();
-        long start = instant.getNano();
-        System.out.println("Результат первого " + expt(5, 10));
-        Clock clock2 = Clock.systemDefaultZone();
-        Instant instant2 = clock2.instant();
-        long intermediate = instant2.getNano();
-        System.out.println("Время выполнения первого в наносекундах " + (intermediate - start));
-        System.out.println("Результат второго " + expt(2, 11));
-        Clock clock3 = Clock.systemDefaultZone();
-        Instant instant3 = clock3.instant();
-        long end = instant3.getNano();
-        System.out.println("Время выполнения второго в наносекундах " + (end - intermediate));
+        long t1 = System.nanoTime();
+        System.out.println("Результат первого " + expt(10, 5));
+        long t2 = System.nanoTime();
+        System.out.println("Время выполнения первого " + (t2 - t1));
+        System.out.println("Результат второго " + expt(10, 9));
+        long t3 = System.nanoTime();
+        System.out.println("Время выполнения второго " + (t3 - t2));
     }
 
     public static void test2() throws InterruptedException {
-        Clock clock = Clock.systemDefaultZone();
-        Instant instant = clock.instant();
-        long start = instant.getNano();
-        System.out.println("Результат первого " + exptCycle(5, 10));
-        Clock clock2 = Clock.systemDefaultZone();
-        Instant instant2 = clock2.instant();
-        long intermediate = instant2.getNano();
-        System.out.println("Время выполнения первого в наносекундах " + (intermediate - start));
-        System.out.println("Результат второго " + exptCycle(2, 11));
-        Clock clock3 = Clock.systemDefaultZone();
-        Instant instant3 = clock3.instant();
-        long end = instant3.getNano();
-        System.out.println("Время выполнения второго в наносекундах " + (end - intermediate));
+        long t1 = System.nanoTime();
+        System.out.println("Результат первого " + exptCycle(10, 5));
+        long t2 = System.nanoTime();
+        System.out.println("Время выполнения первого " + (t2 - t1));
+        System.out.println("Результат второго " + exptCycle(10, 9));
+        long t3 = System.nanoTime();
+        System.out.println("Время выполнения второго " + (t3 - t2));
     }
 
     public static void main(String[] args) throws InterruptedException {
